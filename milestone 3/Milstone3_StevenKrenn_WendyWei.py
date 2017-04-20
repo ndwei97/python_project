02719e521ed43bf8c6b2596e811636272cb14bef
Steven Krenn <Steven.Krenn@icloud.com>
2017-04-19 20:30:15 -0400

    Got rid of some comments

--- milestone 3/Milstone3_StevenKrenn_WendyWei.py
+++ milestone 3/Milstone3_StevenKrenn_WendyWei.py
@@ -62,34 +62,6 @@ def main():
 	# for elem in sorted_x[-30:]:
 	# 	print(elem)
 
-# 	for elem in sorted_x[-30:]:
-# 		print(elem)
-# =======
-
-# 	sorted_counts = sorted(counts.items(), key=operator.itemgetter(1))
-
-# 	# print(sorted_counts)
-
-# #
-# 	import numpy as np
-# 	import scipy.stats as stats
-# 	import matplotlib.pyplot as pl
-# 	h = []
-
-# 	h = sorted(list(counts.values()))
-# 	# print(h)
-# 	# h = sorted([186, 176, 158, 180, 186, 168, 168, 164, 178, 170, 189, 195, 172,
-# 	#      187, 180, 186, 185, 168, 179, 178, 183, 179, 170, 175, 186, 159,
-# 	#      161, 178, 175, 185, 175, 162, 173, 172, 177, 175, 172, 177, 180])  #sorted
-
-# 	# fit = stats.norm.pdf(h, np.mean(h), np.std(h))  #this is a fitting indeed
-
-# 	# pl.plot(h,fit,'-o')
-
-# 	pl.hist(h,normed=True)      #use this to draw histogram of your data
-# >>>>>>> Stashed changes
-
-# 	pl.show()                   #use may also need add this
 
 
 
